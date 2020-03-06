de6fb76a4247e3f88d8a2e646bcca8f201058d57
tangbl93 <tangbl93@gmail.com>
2020-03-06 11:12:30 +0800

    Init commit

---
+++ src/abstractFactory/AbstractFactory.java
@@ -0,0 +1,8 @@
+package abstractFactory;
+
+import factory.Shape;
+
+public abstract class AbstractFactory {
+    public abstract Color getColor(String color);
+    public abstract Shape getShape(String shape);
+}
